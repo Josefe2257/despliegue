31c97deed6de80ed2ca2d709046df66992db6628
Josefe2257 <80032866+Josefe2257@users.noreply.github.com>
2021-03-07 12:56:01 +0100

    Create escuacion.java
    Ejercicio de ecuacion de segundo grado parte 1

---
+++ escuacion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ecuacion {
+    public static void main(String[] args) {
+        int a,b,c;
+        double result;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        result=((-a+Math.sqrt(b*b-4*a*c))/(2*a));
+    }
+}
