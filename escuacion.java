b811923ccd06443cc5fe2935db434ea46fe8fe2d
Josefe2257 <80032866+Josefe2257@users.noreply.github.com>
2021-03-07 13:03:17 +0100

    Update escuacion.java
    segunda parte

--- escuacion.java
+++ escuacion.java
@@ -2,11 +2,12 @@ import java.util.*;
 public class ecuacion {
     public static void main(String[] args) {
         int a,b,c;
-        double result;
+        double result,resultmenos;
         Scanner sc=new Scanner(System.in);
         a=sc.nextInt();
         b=sc.nextInt();
         c=sc.nextInt();
         result=((-a+Math.sqrt(b*b-4*a*c))/(2*a));
+        resultmenos=((-a-Math.sqrt(b*b-4*a*c))/(2*a));
     }
 }
